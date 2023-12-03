133429a7df87896742903f28403fb25af803448b
Bassam Sabry <119759250+BassamSabry1999@users.noreply.github.com>
2023-12-03 04:27:14 +0200

    added all files

---
+++ README.md
@@ -0,0 +1,15 @@
+# My-Personal-Site
+
+This is my personal website. I have used all my skills in this project and added all my projects with continuous updating when building a new project.. Also there are all my contacts.. The site is responsive to all types of screens and devices
+
+### Technologies Used:
+
+---
+
+1. Html
+2. Scss
+3. js
+ 
+
+![preview](https://i.imgur.com/B3aUfN7.png)
+# personal-site
---
+++ js/style_switchir.js
@@ -0,0 +1,59 @@
+// ======================== Style Switcher ======================== //
+const iconSwitch = document.querySelector(".style-switcher-toggler");
+let styleSwitcher = document.querySelector(".style-switcher");
+
+iconSwitch.addEventListener("click", () => {
+  styleSwitcher.classList.toggle("open");
+}),
+  document.addEventListener("keydown", function (e) {
+    if (e.keyCode == 27) {
+      styleSwitcher.classList.remove("open");
+    }
+  });
+
+// ======================== Theme Color Switcher ======================== //
+
+const alternateStyels = document.querySelectorAll(".alternate-style"),
+  setActiveStyle = (e) => {
+    alternateStyels.forEach((t) => {
+      e === t.getAttribute("title")
+        ? t.removeAttribute("disabled")
+        : t.setAttribute("disabled", "true");
+    });
+  };
+// ======================== Light/Night Mode Switcher ======================== //
+
+dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+  dayNight.querySelector("i").classList.toggle("fa-sun"),
+    dayNight.querySelector("i").classList.toggle("fa-moon"),
+    document.body.classList.toggle("dark");
+
+  let imgHome = document.querySelector(".home-img .img");
+  if (dayNight.querySelector("i").classList.contains("fa-sun")) {
+    imgHome.src = "img/photo_2023-02-22_23-00-21-removebg-preview.png";
+    metaDarkMode();
+  } else {
+    imgHome.src = "img/photo_2023-02-22_23-00-21-removebg-preview.png";
+    metaLightMode();
+  }
+});
+
+window.addEventListener("load", () => {
+  document.body.classList.contains("dark")
+    ? dayNight.querySelector("i").classList.add("fa-sun")
+    : dayNight.querySelector("i").classList.add("fa-moon");
+});
+
+// ======================== theme-color ======================== //
+
+const themeColor = document.querySelector(".theme-color");
+
+let metaDarkMode = () => {
+  themeColor.removeAttribute("content");
+  themeColor.setAttribute("content", "#151515");
+};
+
+let metaLightMode = () => {
+  themeColor.setAttribute("content", "#f2f2fc");
+};
